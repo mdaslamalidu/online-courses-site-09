02a54e5d5155938ca596908007123e9ae2930678
Md Aslam Ali <mdaslamali675@gmail.com>
2022-10-12 16:17:08 +0600

    update readme file

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -12,7 +12,7 @@ const Home = () => {
             <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:flex-row lg:justify-between">
                 <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
                     <h1 className="md:text-5xl font-bold sm:text-6xl">Build Your Skills in any 
-                        <span className="dark:text-violet-400"> Courses/Categories</span>
+                        <span className="dark:text-violet-400"> Categories</span>
                     </h1>
                 </div>
                 <div className="flex items-center justify-center w-full p-6 mt-8 mb-9 lg:mt-0 h-10 lg:h-96 xl:h-112 2xl:h-128">
